2ddabd771b480a1364fe1546f21d7fe44547daa1
tiamarie17 <107492167+tiamarie17@users.noreply.github.com>
2022-06-16 16:17:18 -0500

    Delete rotate.js

--- rotate.js
+++
@@ -1,5 +0,0 @@
-const current_rotation = 0;
-  document.querySelector(".rotate").addEventListener('click', function(){
-	current_rotation += 75;
-  document.querySelector(".rotate").style.transform = 'rotate(' + current_rotation + 'deg)';
-});
